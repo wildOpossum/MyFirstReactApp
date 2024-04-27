bcf0c96ff3645ccd92e5cb9c065debb8d91aad8d
Vetal <sonovetal@gmail.com>
2024-04-27 19:21:48 +0300

    modify store. Added a slice to categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pizzas from './slice/pizzaSlice';
+import categories from './slice/categoriesSlice';
 
 export const store = configureStore({
 	reducer: {
-		pizzas
+		pizzas,
+		categories
 	}
 });
 
